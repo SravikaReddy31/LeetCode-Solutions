7b1e296ee7004639b14d79f4242d2556da659391
SravikaReddy31 <sravikareddydanda31@gmail.com>
2026-09-12 10:50:01 +0530

    Add Easy Java Solution for 925. Long Pressed Name from LeetCode

--- LeetCode/Java/Easy/925. Long Pressed Name/Solution.java
+++ LeetCode/Java/Easy/925. Long Pressed Name/Solution.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        int i=0, j=0;
-        while(i<name.length() && j<typed.length()) {
-            if(name.charAt(i) == typed.charAt(j)) {
-                i++;j++;
-            } else if(j>0 && typed.charAt(j) == typed.charAt(j-1)) {
+        int i = 0, j = 0;
+
+        while (i < name.length() && j < typed.length()) {
+
+            if (name.charAt(i) == typed.charAt(j)) {
+                i++;
                 j++;
-            } else if (name.charAt(i) != typed.charAt(j)){
+
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+
+            } else {
                 return false;
             }
         }
-        return true;
+
+        return i == name.length();
     }
 }
