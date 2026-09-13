0046c5e9bf3b18f9ec39a991a9ecf7817a65c740
SravikaReddy31 <sravikareddydanda31@gmail.com>
2026-09-13 21:01:02 +0530

    Add Easy Java Solution for 941. Sort Array By Parity from LeetCode

---
+++ LeetCode/Java/Easy/941. Sort Array By Parity/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParity(int[] arr) {
+        int i=0;
+        int j=arr.length-1;
+        while (i < j) {
+            while (i < j && arr[i] % 2 == 0) {
+            i++;
+        }
+        while (i < j && arr[j] % 2 != 0) {
+            j--;
+        }
+        if (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    return arr;
+    }
+}
