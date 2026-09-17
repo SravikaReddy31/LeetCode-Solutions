25b8b05c7bfc0c059222985aba00e69581278a10
SravikaReddy31 <sravikareddydanda31@gmail.com>
2026-09-17 18:20:53 +0530

    Add Easy Java Solution for 258. Add Digits from LeetCode

--- LeetCode/Java/Easy/258. Add Digits/Solution.java
+++ LeetCode/Java/Easy/258. Add Digits/Solution.java
@@ -1,17 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-        /* while(num >= 10) {
-            int sum = 0;
-            while(num > 0) {
-                int rem = num % 10;
-                sum = sum + rem;
-                num /= 10;
-            }
-            num = sum;
-        }
-        return num; */
         if(num == 0) return 0;
         if(num % 9 == 0) return 9;
         return num % 9;
-    } 
+    }
 }
