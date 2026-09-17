b1c6eb839459fe4c257fd73482140a22bbaf0a88
SravikaReddy31 <sravikareddydanda31@gmail.com>
2026-09-17 18:16:33 +0530

    Add Easy Java Solution for 263. Ugly Number from LeetCode

--- LeetCode/Patterns/Math/263. Ugly Number/Solution.java
+++ LeetCode/Patterns/Math/263. Ugly Number/Solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isUgly(int n) {
-        while(n != 1) {
-            if(n % 2 == 0) 
+        while (n > 0) {
+            if (n % 2 == 0)
                 n /= 2;
-            else if(n % 3 == 0) 
+            else if (n % 3 == 0)
                 n /= 3;
-            else if (n % 5 == 0) 
+            else if (n % 5 == 0)
                 n /= 5;
-            else return false;
+            else
+                break;
         }
+        if (n == 1) return true;
+        else return false;
     }
-    return true;
 }
