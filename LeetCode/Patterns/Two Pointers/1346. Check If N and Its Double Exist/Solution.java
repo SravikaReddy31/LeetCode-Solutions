9bb0af0a75f7882a542dc3919cfdfdd722b77799
SravikaReddy31 <sravikareddydanda31@gmail.com>
2026-09-20 10:55:37 +0530

    Add Easy Java Solution for 1346. Check If N and Its Double Exist from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/1346. Check If N and Its Double Exist/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        int j=arr.length-1;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]*2 == arr[j]) {
+                return true;
+            } 
+        }
+        return false;
+    }
+}
