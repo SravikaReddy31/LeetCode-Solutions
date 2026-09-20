74a7aa432d7b23cbfe260c8e099900a4096bab81
SravikaReddy31 <sravikareddydanda31@gmail.com>
2026-09-20 11:03:03 +0530

    Add Easy Java Solution for 1468. Check If N and Its Double Exist from LeetCode

--- LeetCode/Java/Easy/1468. Check If N and Its Double Exist/Solution.java
+++ LeetCode/Java/Easy/1468. Check If N and Its Double Exist/Solution.java
@@ -6,6 +6,8 @@ class Solution {
             for(int j=0;j<arr.length;j++) {
                 if(d == arr[j]) {
                     return true;
+                } else {
+                    return false;
                 }
             }
         }
